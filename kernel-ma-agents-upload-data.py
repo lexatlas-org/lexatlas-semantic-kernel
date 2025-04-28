330fa1a44a264b1c4f16b7179156a2461a536303
Mario Garcia <mariogarcia.ar@gmail.com>
2025-04-28 19:24:59 -0300

    feat: update main script to utilize upload_files_create_vector_store_return_tool for file processing

--- kernel-ma-agents-upload-data.py
+++ kernel-ma-agents-upload-data.py
@@ -1,9 +1,11 @@
 # main.py
 import asyncio
 from agent_utils import (
+    get_agent_by_id,
     get_project_client,
     get_root_dir,
-    upload_multiple_files_and_create_vector_store,
+    upload_files_create_vector_store_return_tool,
+    # upload_multiple_files_and_create_vector_store,
 )
 from config import agents_config  # (Still imported for future use)
 
@@ -12,31 +14,32 @@ ROOT_DIR = get_root_dir()
 async def main():
     client = get_project_client()
 
-    # agent = client.agents.create_agent(
-    #     model="gpt-4o-mini",
-    #     name="my-agent",
-    #     instructions="You are a helpful agent",
-    #     # tools=file_search_tool.definitions,
-    #     # tool_resources=file_search_tool.resources,
-    # )
-    # agent.update()
-    # client.agents.update_agent(agent.id, agent)
+    agent_id = "asst_tkcxfBRgzYP1rhMIHmU64qFH"  # Replace with your actual agent ID
+    agent = get_agent_by_id(client, agent_id)
 
-    with client:  # Important: use async context
-        file_paths = [
-            ROOT_DIR / "prompts" / "docs" / "regulations" / "doc01.txt",
-            ROOT_DIR / "prompts" / "docs" / "regulations" / "doc02.txt",
-        ]
+    print(f"Running agent: {agent.name} with ID: {agent.id}")
+
+    # with client:  # Important: use async context
+    file_paths = [
+        ROOT_DIR / "prompts" / "docs" / "regulations" / "doc01.txt",
+        ROOT_DIR / "prompts" / "docs" / "regulations" / "doc02.txt",
+    ]
         
-        vector_store_name = "my_vector_store"
+    vector_store_name = "my_vector_store"
 
-        vector_store = await upload_multiple_files_and_create_vector_store(
-            client=client,
-            file_paths=file_paths,
-            vector_store_name=vector_store_name,
-        )
+    file_search_tool = upload_files_create_vector_store_return_tool(
+        client=client,
+        file_paths=file_paths,
+        vector_store_name=vector_store_name,
+    )
 
-        print(f"Vector Store '{vector_store.name}' created with ID: {vector_store.id}")
+    client.agents.update_agent(
+        agent_id=agent.id, 
+        tools=file_search_tool.definitions, 
+        tool_resources=file_search_tool.resources
+    )   
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+ 
