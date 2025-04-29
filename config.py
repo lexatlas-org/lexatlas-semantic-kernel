206b347621355e7be22fd6883c508a37d62c66c7
Mario Garcia <mariogarcia.ar@gmail.com>
2025-04-29 02:56:51 -0300

    refactor: update agents_config to use gpt-4o-mini models for improved consistency

--- config.py
+++ config.py
@@ -2,17 +2,17 @@ from pathlib import Path
 
 DIR_ROOT = Path(__file__).parent
 
+# agents_config = [
+#     {'name': 'ClassifierAgent',      'model': 'gpt-35-turbo-16k',  'instructions': DIR_ROOT / "prompts/v1/1_ClassifierAgentPrompt.txt"},
+#     {'name': 'RegulationRetriever',  'model': 'gpt-35-turbo-16k',  'instructions': DIR_ROOT / "prompts/v1/2_RegulationRetrieverAgentPrompt.txt"},
+#     {'name': 'ComplianceChecker',    'model': 'gpt-4o-mini',  'instructions': DIR_ROOT / "prompts/v1/3_ComplianceCheckerAgentPrompt.txt"},
+#     {'name': 'ReportGenerator',      'model': 'gpt-4o', 'instructions': DIR_ROOT / "prompts/v1/4_ReportGeneratorAgentPrompt.txt"},
+# ]
+
 agents_config = [
-    {'name': 'ClassifierAgent',      'model': 'gpt-35-turbo-16k',  'instructions': DIR_ROOT / "prompts/v1/1_ClassifierAgentPrompt.txt"},
-    {'name': 'RegulationRetriever',  'model': 'gpt-35-turbo-16k',  'instructions': DIR_ROOT / "prompts/v1/2_RegulationRetrieverAgentPrompt.txt"},
+    {'name': 'ClassifierAgent',      'model': 'gpt-4o-mini',  'instructions': DIR_ROOT / "prompts/v1/1_ClassifierAgentPrompt.txt"},
+    {'name': 'RegulationRetriever',  'model': 'gpt-4o-mini',  'instructions': DIR_ROOT / "prompts/v1/2_RegulationRetrieverAgentPrompt.txt"},
     {'name': 'ComplianceChecker',    'model': 'gpt-4o-mini',  'instructions': DIR_ROOT / "prompts/v1/3_ComplianceCheckerAgentPrompt.txt"},
     {'name': 'ReportGenerator',      'model': 'gpt-4o', 'instructions': DIR_ROOT / "prompts/v1/4_ReportGeneratorAgentPrompt.txt"},
 ]
 
-
-# agents_config = [
-#     {'name': 'ClassifierAgent',      'model': 'gpt-4o-mini',  'instructions': DIR_ROOT / "prompts/1_ClassifierAgentPrompt.txt"},
-#     {'name': 'RegulationRetriever',  'model': 'gpt-4o-mini',  'instructions': DIR_ROOT / "prompts/2_RegulationRetrieverAgentPrompt.txt"},
-#     {'name': 'ComplianceChecker',    'model': 'gpt-4o-mini',  'instructions': DIR_ROOT / "prompts/3_ComplianceCheckerAgentPrompt.txt"},
-#     {'name': 'ReportGenerator',      'model': 'gpt-4o-mini', 'instructions': DIR_ROOT / "prompts/4_ReportGeneratorAgentPrompt.txt"},
-# ]
